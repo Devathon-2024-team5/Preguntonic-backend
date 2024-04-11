c1463ce33b6065e3b304fa5d2726f6de3ca4b184
jbaquero <jbaquero@twilio.com>
2024-04-11 10:14:13 -0500

    feat: set-up project with initial configuration

---
+++ src/test/java/com/devathon/preguntonic/PreguntonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devathon.preguntonic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreguntonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
